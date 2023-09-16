2f9aa746472759fecf71e55a7ce7b0e1ecb1df37
emderts <thebmw114@gmail.com>
2023-09-16 10:18:15 +0900

    add toggle

--- js/ui.js
+++ js/ui.js
@@ -1,46 +1,21 @@
 window.onload = function () {
-  var viewAll = localStorage.getItem('viewAll') == 'true';
-  var highlightNew = localStorage.getItem('highlightNew') == 'true';
-  window.toggleViewAll.innerText = viewAll ? '한국어판만 보기' : '모두 보기';
-  window.toggleHighlightNew.innerText = highlightNew ? '강조 끄기' : '신규 항목 강조';
+  var viewAllTaboo = localStorage.getItem('viewAllTaboo') == 'true';
+  window.toggleViewAllTaboo.innerText = viewAllTaboo ? '전체 금기 보기' : '기존 금기 차이점만 보기';
 
-  window.barButton.onclick = function() {
-    toggleViewSelector('ul.barItem', 'block');
+  window.toggleViewAllTaboo.onclick = function() {
+    toggleViewSelector('.revised, .previous');
+    viewAllTaboo = !viewAllTaboo;
+      window.toggleViewAllTaboo.innerText = viewAllTaboo ? '전체 금기 보기' : '기존 금기 차이점만 보기';
+    localStorage.setItem('viewAllTaboo', viewAllTaboo);
   }
 
-  window.toggleViewAll.onclick = function() {
-    toggleViewSelector('.eoep, eoec, .tskp, .tskc');
-    viewAll = !viewAll;
-      window.toggleViewAll.innerText = viewAll ? '한국어판만 보기' : '모두 보기';
-    localStorage.setItem('viewAll', viewAll);
+  if (!viewAllTaboo) {
+    toggleViewSelector('.revised');
+  } else {
+    toggleViewSelector('.previous');
   }
 
-  window.toggleHighlightNew.onclick = function() {
-    toggleViewColor('.V1_4, .V1_41, .new');
-    toggleViewSelector('.newShowList', 'list-item');
-    highlightNew = !highlightNew;
-    window.toggleHighlightNew.innerText = highlightNew ? '강조 끄기' : '신규 항목 강조';
-    localStorage.setItem('highlightNew', highlightNew);
-  }
-
-  var viewSelector = document.querySelectorAll('.scenarioToggleButton');
-  viewSelector.forEach(function (x) {
-    x.onclick = function() {
-      x.innerText = x.innerText == '내용 보기' ? '내용 가리기' : '내용 보기';
-      toggleView(window[x.id.replace('Toggle', 'Div')], 'display', 'block');
-    };
-  });
-
-  if (!viewAll) {
-    toggleViewSelector('.eoep, eoec, .tskp, .tskc');
-  }
-
-  if (highlightNew) {
-    toggleViewColor('.V1_4, .V1_41, .new');
-    toggleViewSelector('.newShowList', 'list-item');
-  }
-
-  putCurrentItemClass(currentItemClass);
+  //putCurrentItemClass(currentItemClass);
 }
 
 function toggleView (element, type, mode, initial) {
